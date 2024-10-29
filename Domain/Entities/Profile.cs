3a551728f445880ebda05e4158e3e3466fdb916c
marbeydotdev <contact@marbey.dev>
2024-10-30 00:58:54 +0100

    + higher limit

--- Domain/Entities/Profile.cs
+++ Domain/Entities/Profile.cs
@@ -6,7 +6,7 @@ public class Profile
 {
     [MaxLength(30)]
     public string DisplayName { get; set; } = "User";
-    [MaxLength(200)]
+    [MaxLength(500)]
     public string ProfilePicture { get; set; } = "/favicon.png";
     [MaxLength(5000)]
     public string Biography { get; set; } = string.Empty;
